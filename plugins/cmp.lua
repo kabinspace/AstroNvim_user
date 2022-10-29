c8ff30d322f5842baef8bbdb2ae0210a644f4dcd
kabinspace <kabinkarki555@gmail.com>
2022-10-30 01:46:43 +0545

    chore(cmp): disable tab completion

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,4 +1,5 @@
 local cmp = require "cmp"
+local luasnip = require "luasnip"
 return {
   completion = {
     completeopt = "menu,menuone,noinsert",
@@ -13,5 +14,19 @@ return {
     ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
     ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
     ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
 }
