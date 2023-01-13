7b9ccafdbca780eb581a8e6ff6cb1e873fbd92d5
Micah Halter <micah@mehalter.com>
2023-01-13 07:12:40 -0500

    feat!: update to Astronvim v3.0.0

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,32 +1,37 @@
-local cmp = require "cmp"
-local luasnip = require "luasnip"
 return {
-  completion = {
-    completeopt = "menu,menuone,noinsert",
-  },
-  window = {
-    documentation = {
-      max_width = 40,
-    },
-  },
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-    ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-    ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require "cmp"
+    local luasnip = require "luasnip"
+    return astronvim.default_tbl({
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      window = {
+        documentation = {
+          max_width = 40,
+        },
+      },
+      mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      },
+    }, opts)
+  end,
 }
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,26 +1,75 @@
 return {
   -- You can disable default plugins as follows:
-  ["goolord/alpha-nvim"] = { disable = true },
-  ["max397574/better-escape.nvim"] = { disable = true },
+  { "goolord/alpha-nvim", enabled = false },
+  { "max397574/better-escape.nvim", enabled = true },
   -- You can also add new plugins here as well:
-  ["wakatime/vim-wakatime"] = {
-    opt = true,
-    setup = function() table.insert(astronvim.file_plugins, "vim-wakatime") end,
-  },
-  ["folke/zen-mode.nvim"] = {
+
+  { "nvim-treesitter/playground", cmd = "TSHighlightCapturesUnderCursor", dependencies = { "nvim-treesitter" } },
+  { "lvimuser/lsp-inlayhints.nvim", config = true },
+  {
+    "folke/zen-mode.nvim",
     cmd = { "ZenMode" },
-    config = function() require("user.plugins.zen-mode").config() end,
+    opts = {
+      window = {
+        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        -- height and width can be:
+        -- * an absolute number of cells when > 1
+        -- * a percentage of the width / height of the editor when <= 1
+        -- * a function that returns the width or the height
+        width = 120, -- width of the Zen window
+        height = 1, -- height of the Zen window
+        -- by default, no options are changed for the Zen window
+        -- uncomment any of the options below, or add other vim.wo options you want to apply
+        options = {
+          -- signcolumn = "no", -- disable signcolumn
+          -- number = false, -- disable number column
+          -- relativenumber = false, -- disable relative numbers
+          -- cursorline = false, -- disable cursorline
+          -- cursorcolumn = false, -- disable cursor column
+          -- foldcolumn = "0", -- disable fold column
+          -- list = false, -- disable whitespace characters
+        },
+      },
+      plugins = {
+        -- disable some global vim options (vim.o...)
+        -- comment the lines to not apply the options
+        options = {
+          enabled = true,
+          ruler = false, -- disables the ruler text in the cmd line area
+          showcmd = false, -- disables the command in the last line of the screen
+        },
+        twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+        gitsigns = { enabled = false }, -- disables git signs
+        tmux = { enabled = false }, -- disables the tmux statusline
+        -- this will change the font size on kitty when in zen mode
+        -- to make this work, you need to set the following kitty options:
+        -- - allow_remote_control socket-only
+        -- - listen_on unix:/tmp/kitty
+        kitty = {
+          enabled = false,
+          font = "+4", -- font size increment
+        },
+      },
+    },
   },
-  ["nvim-treesitter/playground"] = {
-    cmd = "TSHighlightCapturesUnderCursor",
+  {
+    "karb94/neoscroll.nvim",
+    init = function() table.insert(astronvim.file_plugins, "neoscroll.nvim") end,
+    opts = {
+      -- All these keys will be mapped to their corresponding default scrolling animation
+      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      hide_cursor = true, -- Hide cursor while scrolling
+      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      easing_function = nil, -- Default easing function
+      pre_hook = nil, -- Function to run before the scrolling animation starts
+      post_hook = nil, -- Function to run after the scrolling animation ends
+    },
   },
-  ["karb94/neoscroll.nvim"] = {
-    opt = true,
-    setup = function() table.insert(astronvim.file_plugins, "neoscroll.nvim") end,
-    config = function() require("user.plugins.neoscroll").config() end,
-  },
-  ["lvimuser/lsp-inlayhints.nvim"] = {
-    module = "lsp-inlayhints",
-    config = function() require "user.plugins.lsp-inlayhints" end,
+  {
+    "wakatime/vim-wakatime",
+    init = function() table.insert(astronvim.file_plugins, "vim-wakatime") end,
   },
 }
