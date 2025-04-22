83edf1522551e0af086d6adb906f7569f86ea36c
Kabin Karki <kabinspace@gmail.com>
2025-04-22 18:53:47 -0400

    feat(snacks): add minimal dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,7 +3,32 @@ return {
   lazy = false,
   priority = 1000,
   opts = {
-    dashboard = { enabled = false },
+    dashboard = {
+      width = 40,
+      header = false,
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "t", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "e", desc = "File Tree", action = ":Neotree" },
+          { icon = " ", key = "r", desc = "Recents", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "keys", gap = 1, padding = 3 },
+      },
+    },
     notifier = { timeout = 1000 },
+    win = { backdrop = false },
+    picker = {
+      prompt = "   ",
+    },
   },
 }
